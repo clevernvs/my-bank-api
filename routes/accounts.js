be912a29427e3622581fa086e7525ff0f7ec4847
Cleverton <clever.nvs@gmail.com>
2020-10-10 11:06:40 -0300

    Metodo POST

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,5 +1,22 @@
 import express from 'express';
+import { promises as fs, readFile, writeFile } from 'fs';
 
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try {
+    let account = req.body;
+    const data = JSON.parse(await readFile('account.json'));
+
+    account = { id: data.nextId++, ...account };
+    data.account.push(account);
+
+    await writeFile('account.json', JSON.stringify(data, null, 2));
+
+    res.send(account);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
 
 export default router;
