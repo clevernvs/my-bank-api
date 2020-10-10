7d5aaad33b63f675222f91c0165ec8f0f571fd12
Cleverton <clever.nvs@gmail.com>
2020-10-10 17:28:31 -0300

    Inserindo a gravação de logs

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import express from 'express';
+import winston, { silly } from 'winston';
 import accountsRouter from './routes/accounts';
 import { promises as fs } from 'fs';
 
 const { readFile, writeFile } = fs;
 
-global.filename = 'accounts.json'; // <- Definindo o arquivo de forma global
+// <- Definindo o arquivo de forma global
+global.filename = 'accounts.json';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [new winston.transports.Console(), new winston.transports.File({ filename: 'my-bank-api.log' })],
+  format: combine(label({ label: 'my-bank-api' }), timestamp(), myFormat),
+});
+// <- Definindo o arquivo de forma global
 
 const app = express();
 app.use(express.json());
-
 app.use('/account', accountsRouter);
-
 app.listen(3000, async () => {
   try {
     await readFile(global.filename);
-    console.log('API Started!');
+
+    logger.info('API Started!');
   } catch (error) {
     const initialJson = {
       nextId: 1,
@@ -23,10 +35,10 @@ app.listen(3000, async () => {
 
     writeFile(global.filename, JSON.stringify(initialJson))
       .then(() => {
-        console.log('API Started and File Created!');
+        logger.info('API Started and File Created!');
       })
       .catch((err) => {
-        console.log(err);
+        logger.error(err);
       });
   }
 });
--- routes/accounts.js
+++ routes/accounts.js
@@ -14,6 +14,8 @@ router.post('/', async (req, res, next) => {
     await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(account);
+
+    global.logger.info(`POST /account - ${JSON.stringify(account)}`);
   } catch (err) {
     next(err);
   }
@@ -24,6 +26,8 @@ router.get('/', async (req, res, next) => {
     const data = JSON.parse(await readFile(global.filename));
 
     res.send(data);
+
+    global.logger.info('GET /account');
   } catch (err) {
     next(err);
   }
@@ -35,6 +39,8 @@ router.get('/:id', async (req, res, next) => {
     const account = data.accounts.find((account) => account.id === parseInt(req.params.id));
 
     res.send(account);
+
+    global.logger.info('GET /account/:id');
   } catch (err) {
     next(err);
   }
@@ -49,6 +55,8 @@ router.delete('/:id', async (req, res, next) => {
     await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.end();
+
+    global.logger.info(`DELETE /account/:id - ${req.params.id}`);
   } catch (err) {
     next(err);
   }
@@ -58,7 +66,6 @@ router.put('/', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.filename));
-
     const index = data.findIndex((accnt) => accnt.id === account.id);
 
     data.accounts[index] = account;
@@ -66,6 +73,8 @@ router.put('/', async (req, res, next) => {
     await writeFile(global.filename, JSON.stringify(data));
 
     res.send(account);
+
+    global.logger.info(`PUT /account - ${JSON.stringify(account)}`);
   } catch (err) {
     next(err);
   }
@@ -75,7 +84,6 @@ router.patch('/updateBalance', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.filename));
-
     const index = data.findIndex((accnt) => accnt.id === account.id);
 
     data.accounts[index].balance = account.balance;
@@ -83,13 +91,15 @@ router.patch('/updateBalance', async (req, res, next) => {
     await writeFile(global.filename, JSON.stringify(data));
 
     res.send(data.accounts[index]);
+
+    global.logger.info(`PATCH /account/updateBalance - ${JSON.stringify(account)}`);
   } catch (err) {
     next(err);
   }
 });
 
 router.use((err, req, res, next) => {
-  console.log(err);
+  global.logger.error(`${req.method} ${req.baseUrl} - ${err.message}`);
   res.status(400).send({ error: err.message });
 });
 
