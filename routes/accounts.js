9d63d6f3e211ebb84a21b66056cee710aed07587
Cleverton <clever.nvs@gmail.com>
2020-10-10 16:34:42 -0300

    Tratamento de erros

--- routes/accounts.js
+++ routes/accounts.js
@@ -3,7 +3,7 @@ import { promises as fs, readFile, writeFile } from 'fs';
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     let account = req.body;
     const data = JSON.parse(await readFile(global.filename));
@@ -14,33 +14,33 @@ router.post('/', async (req, res) => {
     await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(account);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
+  } catch (err) {
+    next(err);
   }
 });
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.filename));
 
     res.send(data);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
+  } catch (err) {
+    next(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.filename));
     const account = data.accounts.find((account) => account.id === parseInt(req.params.id));
 
     res.send(account);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
+  } catch (err) {
+    next(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.filename));
 
@@ -49,12 +49,12 @@ router.delete('/:id', async (req, res) => {
     await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.end();
-  } catch (error) {
-    res.status(400).send({ error: error.message });
+  } catch (err) {
+    next(err);
   }
 });
 
-router.put('/', async (req, res) => {
+router.put('/', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.filename));
@@ -66,12 +66,12 @@ router.put('/', async (req, res) => {
     await writeFile(global.filename, JSON.stringify(data));
 
     res.send(account);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
+  } catch (err) {
+    next(err);
   }
 });
 
-router.patch('/updateBalance', async (req, res) => {
+router.patch('/updateBalance', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.filename));
@@ -83,9 +83,14 @@ router.patch('/updateBalance', async (req, res) => {
     await writeFile(global.filename, JSON.stringify(data));
 
     res.send(data.accounts[index]);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
+  } catch (err) {
+    next(err);
   }
+});
+
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.status(400).send({ error: err.message });
 });
 
 export default router;
