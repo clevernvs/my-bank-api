e7c6a46e5358096d0fe84b7c10591c6db7a9fc7a
Cleverton <clever.nvs@gmail.com>
2020-10-10 17:52:14 -0300

    Inserindo a validação de campos

--- routes/accounts.js
+++ routes/accounts.js
@@ -6,9 +6,16 @@ const router = express.Router();
 router.post('/', async (req, res, next) => {
   try {
     let account = req.body;
-    const data = JSON.parse(await readFile(global.filename));
+    if (!account.name || account.balance == null) {
+      throw new Error('Name e Balance são obrigatórios');
+    }
 
-    account = { id: data.nextId++, ...account };
+    const data = JSON.parse(await readFile(global.filename));
+    account = {
+      id: data.nextId++,
+      name: account.name,
+      balance: account.balance,
+    };
     data.account.push(account);
 
     await writeFile(global.filename, JSON.stringify(data, null, 2));
@@ -65,12 +72,21 @@ router.delete('/:id', async (req, res, next) => {
 router.put('/', async (req, res, next) => {
   try {
     const account = req.body;
+    if (!account.id || !account.name || account.balance == null) {
+      throw new Error('Id, Name e Balance são obrigatórios');
+    }
+
     const data = JSON.parse(await readFile(global.filename));
+
     const index = data.findIndex((accnt) => accnt.id === account.id);
+    if (index === -1) {
+      throw new Error('Registro não encontrado.');
+    }
 
-    data.accounts[index] = account;
+    data.accounts[index].name = account.name;
+    data.accounts[index].balance = account.balance;
 
-    await writeFile(global.filename, JSON.stringify(data));
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(account);
 
@@ -86,9 +102,17 @@ router.patch('/updateBalance', async (req, res, next) => {
     const data = JSON.parse(await readFile(global.filename));
     const index = data.findIndex((accnt) => accnt.id === account.id);
 
+    if (!account.id || account.balance == null) {
+      throw new Error('Id e Balance são obrigatórios');
+    }
+
+    if (index === -1) {
+      throw new Error('Registro não encontrado.');
+    }
+
     data.accounts[index].balance = account.balance;
 
-    await writeFile(global.filename, JSON.stringify(data));
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(data.accounts[index]);
 
